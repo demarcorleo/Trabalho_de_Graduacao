913b843acd02661a36c90f0cad55b5ca99de6d9a
demarcorleo <demarcorleo@gmail.com>
2026-06-02 16:22:31 -0300

    chore: add chunk configuration variables

--- config/settings.py
+++ config/settings.py
@@ -8,3 +8,5 @@ load_dotenv()
 
 PROMPTS_DIR = Path(os.getenv("PROMPTS_DIR", BASE_DIR / "config/prompts"))
 PROMPT_VERSION  =os.getenv("PROMPT_VERSION", "v1")
+CHUNK_SIZE = int(os.getenv("CHUNK_SIZE", 1000))
+CHUNK_OVERLAP = int(os.getenv("CHUNK_OVERLAP", 200))
